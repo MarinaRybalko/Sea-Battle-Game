aee7e4db31ccf0108fad141dea4cb95dc125e0f7
Marina Rybalko <marinarybalko>
2018-03-12 02:54:59 +0200

    Revert "Added two versions of the Sea Battle game"
    This reverts commit b6529c7bab433cbb18370925fff82469b998ad17.

--- SeaBattleGame-Version-2.0.0/WindowsFormsApplication3/bin/Debug/Resources/dbo.BattlePlayer.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[BattlePlayer] (
-    [Id]           INT        IDENTITY (1, 1) NOT NULL,
-    [Name]         NCHAR (30) NOT NULL,
-    [WinAmount]    INT        NULL,
-    [DefeatAmount] INT        NULL,
-    [Rating]       FLOAT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
